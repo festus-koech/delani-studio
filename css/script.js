64ecfca8986ea7f7c3222e24637fbcb4dae0379e
festus-koech <festuskoech95@gmail.com>
2021-01-02 15:29:36 +0300

    delani

---
+++ css/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $(".logo1").click(function(){
+      $(".design-info").toggle();
+      $(".logo1").toggle();
+  });
+  $(".design-info").click(function(){
+      $(".logo1").toggle();
+      $(".design-info").toggle();
+  });
+  $(".logo2").click(function(){
+      $(".dev-info").toggle();
+      $(".logo2").toggle();
+  })
+  $(".dev-info").click(function(){
+      $(".logo2").toggle();
+      $(".dev-info").toggle();
+  });
+  $(".logo3").click(function(){
+      $(".product-info").toggle();
+      $(".logo3").toggle();
+  })
+  $(".product-info").click(function(){
+      $(".logo3").toggle();
+      $(".product-info").toggle();
+  })
+  });
